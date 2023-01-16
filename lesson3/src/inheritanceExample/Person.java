edf656b8b76f6880cb703c1ac3c928abba86c799
Tish Faroul <lfaroul14@gmail.com>
2023-01-16 00:51:17 -0500

    started lesson3 and created a person class

---
+++ lesson3/src/inheritanceExample/Person.java
@@ -0,0 +1,36 @@
+package inheritanceExample;
+
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName){
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String geLasttName(){
+        return lastName;
+    }
+
+    public void seLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString(){
+        return "Name:" + firstName + " " + lastName;
+    }
+
+
+}
